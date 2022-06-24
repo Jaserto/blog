6cd519311b65dfe2bef9ca4f4ec683ca750ec321
jaserto <javiserna7@gmail.com>
2022-06-24 22:46:42 +0200

    fourth  deployment

--- next.config.js
+++ next.config.js
@@ -9,8 +9,8 @@ const nextConfig = {
     path: '',
   },
   pageExtensions: ['js', 'jsx', 'mdx'],
-  basePath: isProd ? '/blog/' : '',
-  assetPrefix: isProd ? '/blog/' : ''
+  //basePath: isProd ? '/blog/' : '',
+  //assetPrefix: isProd ? '/blog/' : ''
 }
 
 module.exports = nextConfig
