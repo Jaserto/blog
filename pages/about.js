d90db0cd4e0c599a86494835996e8cc55d1ed0d8
Javier Serna <javiserna7@gmail.com>
2023-06-08 22:37:00 +0100

    new branch coomit

--- pages/about.js
+++ pages/about.js
@@ -6,14 +6,18 @@ import styled from 'styled-components';
 const StyledDiv = styled.div`
   color:white;
   max-width: 800px;
+  padding: 0 15px;
   p{
     font-size:15px;
+    line-height:1.75em;
+  }
+  a{
+   color:blue;
+   margin-bottom: 10px;
   }
 `;
 
 const About = () => {
-    //const tag = posts.tags.split(',');
-     // console.log(posts);
       return (
         <>
           <Head>
@@ -29,12 +33,14 @@ const About = () => {
                 <span>Software Developer</span>
               </div>
               <p>
-                I am a Front End developer with industry experience building websites 
+                I am a software engineer with industry experience building websites 
                 and web applications. I specialize in JavaScript and have professional experiences working with Node.JS, React.JS, Angular, TypeScript and React Native.
               </p>
               <p>
-                I also have experience working with MongoDB, MySQL, Java and Bash.
+                I also have experience working with Python, Rust, MongoDB, MySQL, Java and Bash.
+                If you want see more, get in touch with my github.
               </p>
+              <a href="https://github.com/Jaserto">Personal Github.</a>
 
             </StyledDiv>
           </main>
